2b51fdae98c12b7bd5442ea93714dcfc19387c42
Mikhail Sheikin <msh@nuts-agency.ru>
2026-02-07 01:09:32 +0300

    add news

---
+++ js/news.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+    initializer(
+        {
+            target: document.querySelector('.js-news-section')
+        },
+        ({ target }) => {
+            const swiper = target.querySelector('.js-swiper');
+            const swiperWrapper = target.querySelector('.js-swiper-wrapper');
+            const swiperSlide = target.querySelectorAll('.js-swiper-slide');
+
+            MediaQuery.down(MediaQuery.breakpoints.sm, {
+                match: (ctx) => {
+                    target.classList.add('list');
+                    
+                    swiper.classList.remove('swiper');
+                    swiperWrapper.classList.remove('swiper-wrapper');
+                    swiperSlide.forEach((slide) => slide.classList.remove('swiper-slide'));
+
+                    ctx.swiper?.destroy();
+                },
+                unmatch: (ctx) => {
+                    target.classList.add('slider');
+
+                    // swiper.classList.add('swiper');
+                    swiperWrapper.classList.add('swiper-wrapper');
+                    swiperSlide.forEach((slide) => slide.classList.add('swiper-slide'));
+
+                    ctx.swiper = new Swiper(swiper, {
+                        slidesPerView: 'auto',
+                        spaceBetween: 26,
+                        navigation: {
+                            prevEl: target.querySelector('.js-button-prev'),
+                            nextEl: target.querySelector('.js-button-next'),
+                        }
+                    });
+                },
+            });
+        },
+    );
+});
