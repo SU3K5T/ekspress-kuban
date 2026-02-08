6e0e48a8a0357257e2a2ba267fc6b53186a4049e
SU3K5T <surikat0013@icloud.com>
2026-02-08 16:57:23 +0300

    fix

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 class ModalMenu extends ModalAppDefault {
     constructor(options) {
-            super({ ...options }
-        );
+        super({ ...options });
+        this.activeSubmenu = null; 
     }
 }
 
@@ -10,38 +10,57 @@ class SubmenuItem {
         this.target = options.target;
         this.name = this.target.getAttribute('submenu');
         this.triggers = document.querySelectorAll(`[submenu-trigger="${this.name}"]`);
+        this.modal = options.modal; 
         this.init();
     }
 
     init() {
-       this.triggers.forEach(trigger => {
-            trigger.addEventListener('click', () => {
+        this.triggers.forEach(trigger => {
+            trigger.addEventListener('click', (e) => {
+                e.preventDefault();
                 this.toggle();
             });
-       });
+        });
     }
 
     toggle() {
-        this.target.classList.toggle('opened');
+        if (this.modal.activeSubmenu && this.modal.activeSubmenu !== this) {
+            this.modal.activeSubmenu.hide();
+        }
+        
+        if (this.target.classList.contains('opened')) {
+            this.hide();
+        } else {
+            this.show();
+        }
     }
 
     show() {
         this.target.classList.add('opened');
+        this.modal.activeSubmenu = this; 
     }
 
     hide() {
         this.target.classList.remove('opened');
+        if (this.modal.activeSubmenu === this) {
+            this.modal.activeSubmenu = null;
+        }
     }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     const modalEl = document.getElementById('menu-modal');
-
-    const modal = new ModalMenu({target: modalEl});
+    const modal = new ModalMenu({ target: modalEl });
+    
     modalEl.querySelectorAll('[submenu]').forEach(submenu => {
-        const item = new SubmenuItem({target: submenu});
+        const item = new SubmenuItem({ 
+            target: submenu,
+            modal: modal
+        });
+        
         modal.subscribe('close', () => {
             item.hide();
+            modal.activeSubmenu = null;
         });
     });
 });
