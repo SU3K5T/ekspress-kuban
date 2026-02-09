b782b90935d30865c3d94c73c48a40aa333d5e24
Mikhail Sheikin <msh@nuts-agency.ru>
2026-02-09 11:04:58 +0300

    fix form

--- js/src/form.js
+++ js/src/form.js
@@ -48,7 +48,7 @@ class FeedbackForm extends Form {
 
   function _phoneInputMaskHelper(input) {
     var maskOptions = {
-      mask: '+{7} | 000 000-00-00',
+      mask: '+{7} (000) 000-00-00',
       prepare: function (appended, masked) {
         if (appended === '8' && masked.value === '') {
           return ' ';
